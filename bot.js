cfc47d1e6aa3d61803c6eb08c13fbab4b21e7e71
QFQFSGFHQ <46243232+QFQFSGFHQ@users.noreply.github.com>
2018-12-29 04:23:04 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("528535365959811099")
+setInterval(function() {
+channel.send(`alah akber`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
